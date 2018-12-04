06e9c09a50c0a779d1261e1d362c8f23bc4e59e0
Paweł Gajda <pawegio@gmail.com>
2018-12-04 22:21:43 +0100

    #Day1 Get first frequency reached twice

--- src/main/Day1.kt
+++ src/main/Day1.kt
@@ -1,12 +1,39 @@
+@file:JvmName("Day1")
+
+import kotlin.system.measureTimeMillis
+
 /**
  * @author pawegio
  */
 
 fun main(args: Array<String>) {
     val input = args.first()
-    val changes = input.splitToSequence(' ')
+    val changes = input.splitToSequence(' ').map(String::toInt)
+
+    // Task 1
     val frequency = getResultingFrequency(changes)
     println("Resulting frequency: $frequency")
+
+    // Task 2
+    val task2Time = measureTimeMillis {
+        val firstFrequencyReachedTwice = getFirstFrequencyReachedTwice(changes)
+        print("First frequency reached twice: $firstFrequencyReachedTwice")
+    }
+    println(" (in $task2Time ms)")
 }
 
-fun getResultingFrequency(changes: Sequence<String>) = changes.sumBy(String::toInt)
+fun getResultingFrequency(changes: Sequence<Int>) = changes.sum()
+
+fun getFirstFrequencyReachedTwice(changes: Sequence<Int>): Int {
+    val reached = mutableSetOf(0)
+    var result = 0
+    var iterator = changes.iterator()
+    var isReachedTwice = false
+    while (!isReachedTwice) {
+        if (!iterator.hasNext()) iterator = changes.iterator()
+        result += iterator.next()
+        if (reached.contains(result)) isReachedTwice = true
+        else reached.add(result)
+    }
+    return result
+}
--- src/test/Day1Test.kt
+++ src/test/Day1Test.kt
@@ -8,13 +8,25 @@ class Day1Test {
 
     @Test
     fun `sum all changes`() {
-        val changes = sequenceOf("+1", "+2", "+3")
+        val changes = sequenceOf(1, 2, 3)
         assertEquals(6, getResultingFrequency(changes))
     }
 
     @Test
     fun `subtract changes with minus sign`() {
-        val changes = sequenceOf("+1", "-2", "-3")
+        val changes = sequenceOf(1, -2, -3)
         assertEquals(-4, getResultingFrequency(changes))
+    }
+
+    @Test
+    fun `get first frequency reached twice`() {
+        listOf(
+            sequenceOf(1, -1) to 0,
+            sequenceOf(3, 3, 4, -2, -4) to 10,
+            sequenceOf(-6, 3, 8, 5, -6) to 5,
+            sequenceOf(7, 7, -2, -7, -4) to 14
+        ).forEach { (changes, expected) ->
+            assertEquals(expected, getFirstFrequencyReachedTwice(changes))
+        }
     }
 }
