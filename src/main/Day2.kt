65f02c78e473546fa8a8e946a8922d549d48d56f
Paweł Gajda <pawegio@gmail.com>
2018-12-07 22:20:22 +0100

    #Day2 Remove redundant log

--- src/main/Day2.kt
+++ src/main/Day2.kt
@@ -25,7 +25,6 @@ fun main(args: Array<String>) {
 
 fun getChecksum(ids: Sequence<String>): Int {
     val counts = ids.map { id -> id.groupingBy { it }.eachCount() }
-    println(counts.toList())
     val twos = counts.count { it.containsValue(2) }
     val threes = counts.count { it.containsValue(3) }
     return twos * threes
